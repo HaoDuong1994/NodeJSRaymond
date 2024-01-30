f492b71c1adb7f9987ffadea3cbd15d957520dc5
HaoDuong1994 <hao.dv5370@gmail.com>
2024-01-30 18:15:19 +0700

    git update

--- src/modules/khanhhang.js
+++ src/modules/khanhhang.js
@@ -13,6 +13,10 @@ const khanhHangSchema = new Schema(
   { timestamps: true } // create Time create, update
 );
 khanhHangSchema.plugin(mongoose_delete, { overrideMethods: "all" });
+khanhHangSchema.pre("save", function (next) {
+  console.log("before saveeeeeeee middle ware");
+  next();
+});
 const khachHang = mongoose.model("KhachHang", khanhHangSchema);
 
 module.exports = khachHang;
